b69a010b94c1a04ac72a11a6ae136f1386eba0d1
kaizizzzzzz <ky427@cornell.edu>
2024-11-04 22:00:55 -0500

    add tokenizer

---
+++ decode.py
@@ -0,0 +1,12 @@
+from tokenization_bitnet import BitnetTokenizer 
+import argparse
+import numpy
+import torch
+
+output_id = numpy.fromfile("encoded_prompt.bin", dtype=numpy.int64)
+output_id = torch.tensor(output_id)
+if output_id[0] == 1: #start token
+    output_id = output_id[1:]
+tokenizer = BitnetTokenizer.from_pretrained('1bitLLM/bitnet_b1_58-large', use_fast=False, cache_dir="tokenizer_model")
+tokens = tokenizer.decode(output_id)
+print(tokens)
---
+++ encode.py
@@ -0,0 +1,13 @@
+from tokenization_bitnet import BitnetTokenizer 
+import argparse
+import numpy
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--prompt', type=str)
+
+args = parser.parse_args()
+tokenizer = BitnetTokenizer.from_pretrained('1bitLLM/bitnet_b1_58-large', use_fast=False, cache_dir="tokenizer_model")
+id = tokenizer(args.prompt, return_tensors="pt").input_ids
+id = id[0].cpu().numpy()
+id.tofile("encoded_prompt.bin")
+breakpoint()
